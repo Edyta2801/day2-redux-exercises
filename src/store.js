a86b2e31725f97301e1ef631dd86bbd1cc18a965
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-05 10:09:29 +0100

    clear files ad create store.js

---
+++ src/store.js
@@ -0,0 +1,30 @@
+import { createStore, combineReducers } from 'redux'
+
+
+
+const reducer1 = () => {
+    return {
+        name: 'Ala'
+    }
+}
+
+
+const reducer2 = () => {
+    return {
+        name: 'Ola'
+    }
+}
+
+const rootReducers({
+    reducersWithoutShortcut: reducer1,
+    reducer2
+})
+
+
+export const store = createStore(
+    rootReducer
+)
+
+console.log(store)
+console.log(store/getState()) 
+console.log(rootReducer)
