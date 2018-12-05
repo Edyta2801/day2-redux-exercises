e2b162b6638996b1719fc249bc6d4f528f8e7dd6
Edyta Szarowska <edyta.szarowska@interia.pl>
2018-12-05 14:21:08 +0100

    Dispatching actions in componentDidMount

--- src/RandomUsers.js
+++ src/RandomUsers.js
@@ -1,30 +1,45 @@
 import React from 'react'
+
 import { connect } from 'react-redux'
+import { fetchUsersAsyncAction } from './state/randomUsers';
 
-const RandomUsers = (props) => (
-    <div>
-        {
-            props._isError ?
-                'Sorry, an error!'
-                :
-                props.isFetching ?
-                    'Loading...'
-                    :
+class RandomUsers extends React.Component {
+    componentDidMount() {
+        this.props._fetchUsersAsyncAction(
+            'http"//randomuser.me/api'
+        )
+
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this._isError ?
+                        'Sorry, an error!'
+                        :
+                        this.isFetching ?
+                            'Loading...'
+                            :
+                            this._users.map(
+                                user => <div>{user.name.first}</div>
+                            )
+                }
+            </div>
+        )
+    }
+}
 
 
-                    props._users.map(
-                        user => <div>{user.name.first}</div>
-                    )
-        }
-    </div>
-)
 const mapStateToProps = state => ({
     _users: state.randomUsers.users,
     _isError: state.randomUsers.isError,
     _isFetching: state.randomUsers.isError,
 })
 
-const mapDispatchToProps = dispatch => ({})
+const mapDispatchToProps = dispatch => ({
+    _fetchUsersAsyncAction: url => dispatch(fetchUsersAsyncAction(url))
+})
 
 
 export default connect(
